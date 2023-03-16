cf2353b9b9f7de6d8a48fab6c93a1935f64ba312
George Hill <georgeophill@outlook.com>
2023-03-16 15:40:55 +0000

    header socials

--- src/components/header/headersocials.jsx
+++ src/components/header/headersocials.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
 
-const headersocials = () => {
+const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/george-hill-web-developer/" target="_blank"></a>
-      <a href="https://github.com/georgeophill" target="_blank"></a>
-      <a href="https://dribbble.com/" target="_blank"></a>
+      <a href="https://www.linkedin.com/in/george-hill-web-developer/" target="_blank"><BsLinkedin/></a>
+      <a href="https://github.com/georgeophill" target="_blank"><FaGithub/></a>
+      <a href="https://dribbble.com/" target="_blank"><FiDribbble/></a>
     </div>
   )
 }
 
-export default headersocials
+export default HeaderSocials
