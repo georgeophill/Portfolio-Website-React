439d6faf6b7e959253d1703b85eecb0a2ae9d21d
George Hill <georgeophill@outlook.com>
2023-03-16 17:22:26 +0000

    navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {GiFiles} from 'react-icons/gi'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const nav = () => {
   return (
-    <div> Nav  </div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><GiFiles/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+
+    </nav>
   )
 }
 
