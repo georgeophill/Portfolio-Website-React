c7820ad5623602dcc4ce6d799131c9d6c33d9973
George Hill <georgeophill@outlook.com>
2023-03-20 15:34:54 +0000

    experience css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -14,35 +14,35 @@ const Experience = () => {
               <h3>Frontend Development</h3>
               <div className="experience__content">
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
                     <h4>HTML</h4>
                     <small className='text-light'>Intermediate</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
                     <h4>CSS</h4>
                     <small className='text-light'>Intermediate</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
                     <h4>JavaScript</h4>
                     <small className='text-light'>Basic</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
                     <h4>Bootstrap</h4>
                     <small className='text-light'>Basic</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
                     <h4>React.JS</h4>
                     <small className='text-light'>Experienced</small>
@@ -56,41 +56,26 @@ const Experience = () => {
             <h3>Backend Development</h3>
               <div className="experience__content">
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
-                  <h4>HTML</h4>
+                  <h4>Ruby</h4>
                   <small className='text-light'>Intermediate</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
-                  <h4>CSS</h4>
-                  <small className='text-light'>Intermediate</small>
-                  </div>
-                </article>
-                <article className='experience__details'>
-                  <BsPatchCheckFill/>
-                  <div>
-                    <h4>JavaScript</h4>
+                    <h4>Node.js</h4>
                     <small className='text-light'>Basic</small>
                   </div>
                 </article>
                 <article className='experience__details'>
-                  <BsPatchCheckFill/>
+                  <BsPatchCheckFill className='experience__details-icon'/>
                   <div>
-                  <h4>Bootstrap</h4>
-                  <small className='text-light'>Basic</small>
+                    <h4>MySQL</h4>
+                    <small className='text-light'>Basic</small>
                   </div>
                 </article>
-                <article className='experience__details'>
-                  <BsPatchCheckFill/>
-                  <div>
-                  <h4>React</h4>
-                  <small className='text-light'>Experienced</small>
-                  </div>
-                </article>
-
               </div>
             </div>
           </div>
