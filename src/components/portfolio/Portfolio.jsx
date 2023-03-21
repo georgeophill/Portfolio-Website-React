7d17d33c4d4b4cccebdccd627f51d02043c6bead
George Hill <georgeophill@outlook.com>
2023-03-21 10:16:47 +0000

    portfolio hash array added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/Eveentmonkey.png'
-import IMG2 from '../../assets/Eveentmonkey.png'
+import IMG2 from '../../assets/lechef1.png'
 import IMG3 from '../../assets/Eveentmonkey.png'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'EventMonkey - Find Events You Love',
+    github: 'https://github.com/iliaswa/EventMonkey',
+    demo: 'http://www.eventmonkey.fun/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'LeChef - Book Your Own Private Chef',
+    github: 'https://github.com/iliaswa/LeChef1',
+    demo: 'http://www.eventmonkey.fun/',
+  },
+
+  // ===== ADD FUTURE PORTFOLIO PROJECTS HERE ========
+
+  // {
+  //   id: 3,
+  //   image: IMG2,
+  //   title: 'LeChef - Book Your Own Private Chef',
+  //   github: 'https://github.com/iliaswa/LeChef1',
+  //   demo: 'http://www.eventmonkey.fun/',
+  // },
+  // {
+  //   id: 4,
+  //   image: IMG2,
+  //   title: 'LeChef - Book Your Own Private Chef',
+  //   github: 'https://github.com/iliaswa/LeChef1',
+  //   demo: 'http://www.eventmonkey.fun/',
+  // },
+]
 
 const Portfolio = () => {
   return (
@@ -12,30 +45,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-           <div className="portfolio__item-image">
-              <img src={IMG1} alt="EventMonkey index page" />
-           </div>
-           <h3>EventMonkey</h3>
-           <a href="https://github.com/iliaswa/EventMonkey" className='btn' target='_blank'>Github</a>
-           <a href="http://www.eventmonkey.fun/" className='btn btn-primary' target='_blank'>Demo</a>
-        </article>
-        <article className='portfolio__item'>
-           <div className="portfolio__item-image">
-              <img src={IMG2} alt="LeChef index page" />
-           </div>
-           <h3>LeChef</h3>
-           <a href="https://github.com/iliaswa/LeChef1" className='btn' target='_blank'>Github</a>
-           <a href="" className='btn btn-primary' target='_blank'>Demo</a>
-        </article>
-        <article className='portfolio__item'>
-           <div className="portfolio__item-image">
-              <img src={IMG3} alt="LeChef index page" />
-           </div>
-           <h3>LeChef</h3>
-           <a href="https://github.com/iliaswa/LeChef1" className='btn' target='_blank'>Github</a>
-           <a href="" className='btn btn-primary' target='_blank'>Demo</a>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                    <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
