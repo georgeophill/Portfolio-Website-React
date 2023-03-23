e8d4fda11d8106ca2a489af7048d1c1af155cbde
George Hill <georgeophill@outlook.com>
2023-03-23 10:19:56 +0000

    about me photo updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me.png'
+import ME from '../../assets/New/on-laptop-cropped.PNG'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
