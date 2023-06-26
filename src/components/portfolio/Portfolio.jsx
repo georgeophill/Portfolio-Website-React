8dd29da8444c61ab0b3d55abe453d5fc6447b4d0
George Hill <georgeophill@outlook.com>
2023-06-26 11:22:28 +0100

    added Bump

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,39 +3,41 @@ import './portfolio.css'
 import IMG1 from '../../assets/Eveentmonkey.png'
 import IMG2 from '../../assets/lechef1.png'
 import IMG3 from '../../assets/flashy.png'
+import IMG4 from '../../assets/bump.png'
 
 const data = [
   {
     id: 1,
+    image: IMG4,
+    title: 'Bump - A Path to Parenthood',
+    github: 'https://github.com/Chey-DB/bump-backend',
+    github1: 'https://github.com/Chey-DB/bump-frontend',
+    demo: 'https://bump-kwkn.onrender.com/',
+  },
+  {
+    id: 2,
     image: IMG1,
     title: 'EventMonkey - Find Events You Love',
     github: 'https://github.com/iliaswa/EventMonkey',
     demo: 'http://www.eventmonkey.fun/',
   },
   {
-    id: 2,
+    id: 3,
     image: IMG2,
     title: 'LeChef - Book Your Own Private Chef',
     github: 'https://github.com/iliaswa/LeChef1',
     demo: '',
   },
-
+  
   // ===== ADD FUTURE PORTFOLIO PROJECTS HERE ========
-
+  
   {
-    id: 3,
+    id: 4,
     image: IMG3,
     title: 'Flashcard App',
     github: 'https://github.com/PiroAvni/Educational_APP_Server_MongoDB',
     demo: 'https://educational-app.onrender.com/',
   },
-  // {
-  //   id: 4,
-  //   image: IMG2,
-  //   title: 'LeChef - Book Your Own Private Chef',
-  //   github: 'https://github.com/iliaswa/LeChef1',
-  //   demo: 'http://www.eventmonkey.fun/',
-  // },
 ]
 
 const Portfolio = () => {
@@ -46,7 +48,7 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, github, github1, demo}) => {
             return (
               <article key={id} className='portfolio__item'>
                 <div className="portfolio__item-image">
@@ -55,6 +57,9 @@ const Portfolio = () => {
                 <h3>{title}</h3>
                 <div className="portfolio__item-cta">
                 <a href={github} className='btn' target='_blank'>Github</a>
+                { id=1 && github1 && ( 
+                <a href={github1} className='btn' target='_blank'>Github(Client-Side)</a>
+                )}
                 <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
                 </div>
               </article>
